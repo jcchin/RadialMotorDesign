16772c1d757a67567f978cd95d540512ee556010
garobed1 <garo.bedonian@gmail.com>
2019-06-24 14:18:50 -0400

    some opt work

--- motor_weight_optimization.py
+++ motor_weight_optimization.py
@@ -9,7 +9,7 @@ class efficiency(ExplicitComponent):
     def setup(self):
         self.add_input('i', 30, units='A', desc='RMS current')
         self.add_input('tq', 25, units='N*m', desc='torque')
-        self.add_input('v', 500, units='V', desc='RMS voltage')
+        self.add_input('v', 385, units='V', desc='RMS voltage')
         self.add_input('rm', 5400, units='rpm', desc='motor speed')
 
         self.add_output('P_in', units='W', desc='input power')
@@ -406,13 +406,13 @@ if __name__ == "__main__":
     #optimization setup
     p.driver = ScipyOptimizeDriver()
     p.driver.options['optimizer'] = 'SLSQP'
-    p.driver.options['debug_print'] = ['desvars', 'objs']
+    p.driver.options['disp'] = True
     model.add_objective('nu', ref=-1)
-    model.add_design_var('gap', lower=.001, upper=.003)
-    model.add_design_var('mot_or', lower=.07, upper=.10)
-    model.add_design_var('rot_or', lower = .05, upper=.068)
+    model.add_design_var('gap', lower=.001, upper=.004)
+    model.add_design_var('mot_or', lower=.075, upper=.10)
+    model.add_design_var('rot_or', lower = .04, upper=.07)
     model.add_design_var('l_st', lower = .0004, upper = .003)
-    model.add_constraint('tq', lower=20, scaler=1)
+    model.add_constraint('tq', lower=12, scaler=1)
     model.add_constraint('size.j', upper=15, scaler=1)
     
     p.setup()
@@ -421,7 +421,7 @@ if __name__ == "__main__":
     p['mot_or'] = 0.08
     p['rot_or'] = 0.06
     p['l_st'] = 0.0009
-    
+    # 
     p.final_setup()
     #p.check_partials(compact_print=True)
     p.model.list_outputs(implicit=True)
@@ -433,7 +433,7 @@ if __name__ == "__main__":
     # print('Rotor Outer Radius................',  p.get_val('balance.rot_or', units='mm'))
     print('Rotor Inner Radius................',  p.get_val('rot_ir', units='mm'))
     print('Rotor Outer Radius................',  p.get_val('rot_or', units='mm'))
-
+    print('Gap...............................',  p.get_val('gap', units='mm'))
     print('Stator Inner Radius...............',  p.get_val('sta_ir', units='mm'))
     print('Motor Outer Radius................',  p.get_val('mass.mot_or', units='mm'))
 
@@ -450,6 +450,8 @@ if __name__ == "__main__":
     print('Mass of Magnets...................',  p.get_val('mag_mass', units='kg'))    
     print('Current Density...................',  p.get_val('size.j'))
     print('Stack Length......................',  p.get_val('mass.l_st', units='mm'))
+    print('Power In..........................',  p.get_val('efficiency.P_in', units='W'))
+    print('Power Out..........................',  p.get_val('efficiency.P_out', units='W'))
     
     print('Efficiency........................',  p.get_val('nu'))
 
