added derivatives for size_comp, gives errors for lines 146 and 156 of incorrect syntax.

electromagnetics/performance_comp.py
@@ -23,10 +23,10 @@ class TorqueComp(om.ExplicitComponent):
        self.add_input('rpm', 5000*np.ones(nn), units='rpm', desc='Rotational Speed')
        self.add_input('stack_length', .0345, units='m', desc='stack length')
 
-       self.add_output('omega', 1000*np.ones(nn), units='Hz', desc='mechanical rad/s')     
+       self.add_output('omega', 900*np.ones(nn), units='Hz', desc='mechanical rad/s')     
        self.add_output('Tq_shaft', 25*np.ones(nn), units='N*m', desc='torque')
-       self.add_output('rot_volume', 0.2, units='m**3', desc='rotor volume')
-       self.add_output('stator_surface_current', 51860, units='A/m', desc='specific electrical loading')
+       self.add_output('rot_volume', .02, units='m**3', desc='rotor volume')
+       self.add_output('stator_surface_current', 50, units='A/m', desc='specific electrical loading')
        self.add_output('Tq_max', 30*np.ones(nn), units='N*m', desc='max torque available')
        
        # r = c = np.arange(nn)  # for scalar variables only
@@ -90,10 +90,10 @@ class EfficiencyComp(om.ExplicitComponent):
     def setup(self):
         nn = self.options['num_nodes']
         self.add_input('P_wire', 500*np.ones(nn), units='W', desc='copper losses')
-        self.add_input('P_steinmetz', 500*np.ones(nn), units='W', desc='iron losses')  
+        self.add_input('P_steinmetz', 200*np.ones(nn), units='W', desc='iron losses')  
         self.add_input('P_shaft', 14000*np.ones(nn), units='W', desc='output power') 
         self.add_input('Tq_shaft', 30*np.ones(nn), units='N*m', desc='torque') 
-        self.add_input('omega', 1000*np.ones(nn), units='Hz', desc='mechanical rad/s')  
+        self.add_input('omega', 400*np.ones(nn), units='Hz', desc='mechanical rad/s')  
         self.add_input('rpm', 5000*np.ones(nn), units='rpm', desc='speed of prop')   
 
         self.add_output('P_in', 15*np.ones(nn), units='kW', desc='input power')

thermal/motor_losses.py
@@ -16,31 +16,29 @@ class WindingLossComp(om.ExplicitComponent):
         nn = self.options['num_nodes']
         self.add_input('rpm', 4000*np.ones(nn), units='rpm', desc='Rotation speed')
         self.add_input('n_m', 20, desc='Number of magnets')
-        self.add_input('mu_o', 0.4*pi*10**-6, units='H/m', desc='permeability of free space')    
+        self.add_input('mu_o', 1.2566e-6, units='H/m', desc='permeability of free space')    
         self.add_input('mu_r', 1.0, units='H/m', desc='relative magnetic permeability of ferromagnetic materials') 
-        self.add_input('r_strand', 0.0005, units='m', desc='radius of one strand of litz wire')
+        self.add_input('r_strand', 0.0001605, units='m', desc='radius of one strand of litz wire')
         self.add_input('T_windings', 150, units='C', desc='operating temperature of windings')
         self.add_input('T_coeff_cu', 0.00393, desc='temperature coefficient for copper')
         self.add_input('resistivity_wire', 1.724e-8, units='ohm*m', desc='resisitivity of Cu at 20 degC')
         self.add_input('I', 30*np.ones(nn), units='A', desc='RMS current into motor')
         self.add_input('stack_length', 0.035, units='m', desc='axial length of stator')
-        self.add_input('n_slots', 20, desc='number of slots')
+        self.add_input('n_slots', 24, desc='number of slots')
         self.add_input('n_turns', 12, desc='number of winding turns')
         self.add_input('n_strands', 41, desc='number of strands in litz wire')        
         self.add_input('AC_power_factor', 0.5*np.ones(nn), desc='litz wire AC power factor')
 
-        self.add_output('f_e', 1000*np.ones(nn), units = 'Hz', desc='electrical frequency')
-        self.add_output('r_litz', 0.002, units='m', desc='radius of whole litz wire')
+        self.add_output('f_e', 900*np.ones(nn), units = 'Hz', desc='electrical frequency')
+        self.add_output('r_litz', 0.0011, units='m', desc='radius of whole litz wire')
         self.add_output('L_wire', 10, units='m', desc='length of wire for one phase')
         self.add_output('temp_resistivity', 1.724e-8, units='ohm*m', desc='temp dependent resistivity')
         self.add_output('R_dc', 1, units='ohm', desc= 'DC resistance')
         self.add_output('skin_depth', 0.001*np.ones(nn), units='m', desc='skin depth of wire')
         self.add_output('A_cu', .005, units='m**2', desc='total area of copper in one slot')
-        self.add_output('P_dc', 100*np.ones(nn), units='W ', desc= 'Power loss from dc resistance')
+        self.add_output('P_dc', 277*np.ones(nn), units='W ', desc= 'Power loss from dc resistance')
         self.add_output('P_ac', 100*np.ones(nn), units='W ', desc= 'Power loss from ac resistance')
         self.add_output('P_wire', 400*np.ones(nn), units='W ', desc= 'total power loss from wire')
-        self.add_output('P_cu', 500, units='W', desc='copper losses')           # delete output
-
 
         r = c = np.arange(nn)  # for scalar variables only
         self.declare_partials('*' , '*', method='fd')
@@ -73,7 +71,7 @@ class WindingLossComp(om.ExplicitComponent):
         n_strands = inputs['n_strands']
         AC_pf = inputs['AC_power_factor']
 
-        outputs['f_e']              = n_m / 2 * rpm / 60    # Eqn 1.5 "Brushless PM Motor Design" by D. Hansleman                                       
+        outputs['f_e']              = n_m / 2 * rpm / 60                                            # Eqn 1.5 "Brushless PM Motor Design" by D. Hansleman                                       
         outputs['r_litz']           = (np.sqrt(n_strands) * 1.154 * r_strand*2)/2                   # New England Wire
         outputs['L_wire']           = (n_slots/3 * n_turns) * (stack_length*2 + .017*2)              
         outputs['temp_resistivity'] = (resistivity_wire * (1 + T_coeff_cu*(T_windings-20)))         # Eqn 4.14 "Brushless PM Motor Design" by D. Hansleman
@@ -91,13 +89,13 @@ class SteinmetzLossComp(om.ExplicitComponent):
 
     def setup(self):
         nn = self.options['num_nodes']
-        self.add_input('f_e', 1000*np.ones(nn), units='Hz', desc='Electrical frequency')
+        self.add_input('f_e', 900*np.ones(nn), units='Hz', desc='Electrical frequency')
         self.add_input('B_pk', 2.05, units='T', desc='Peak magnetic field in Tesla')
         self.add_input('alpha_stein', 1.286, desc='Alpha coefficient for steinmetz, constant')
         self.add_input('beta_stein', 1.76835, desc='Beta coefficient for steinmentz, dependent on freq')  
         self.add_input('k_stein', 0.0044, desc='k constant for steinmentz')
         self.add_input('motor_mass', 2, units='kg', desc='total mass of back-iron')
-        self.add_output('P_steinmetz', 400*np.ones(nn), units='W', desc='Simplified steinmetz losses')
+        self.add_output('P_steinmetz', 200*np.ones(nn), units='W', desc='Simplified steinmetz losses')
 
         r = c = np.arange(nn)  # for scalar variables only
         self.declare_partials('*' , '*', method='fd')